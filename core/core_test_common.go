0045b96ad412c1d64e6f86eb56507081b548fccf
hiroyuki-takayama-RAIX <takayama_hiroyuki@retail-ai.jp>
2023-09-13 18:02:33 +0900

    todo.ymlの削除、テストコードのリファクタリング

--- core/core_test_common.go
+++ core/core_test_common.go
@@ -14,10 +14,17 @@ func BeforeEach() error {
 	defer tx.Rollback()
 
 	q := `
-	INSERT INTO customer (id, username, addr, phone) VALUES (1001, 'John', 'Los Angeles, California', '(213) 555 0147');		
-	INSERT INTO account (id, balance) VALUES (1001, 100);
-	INSERT INTO customer (id, username, addr, phone) VALUES (3003, 'Ide Non No', 'Ta No Tsu', '(0120) 117 117');		
-	INSERT INTO account (id, balance) VALUES (3003, 100);
+	INSERT INTO customer (id, username, addr, phone) 
+	VALUES (1001, 'John', 'Los Angeles, California', '(213) 555 0147');
+
+	INSERT INTO account (id, balance) 
+	VALUES (1001, 100);
+
+	INSERT INTO customer (id, username, addr, phone) 
+	VALUES (3003, 'Ide Non No', 'Ta No Tsu', '(0120) 117 117');
+
+	INSERT INTO account (id, balance) 
+	VALUES (3003, 100);
     `
 	_, err = tx.ExecContext(context.Background(), q)
 	if err != nil {
